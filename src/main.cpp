a0e850f75630520c8b44e6d7c1469d2db017d352
Marcos <marcosgarralaga@gmail.com>
2022-11-09 00:15:54 +0100

    Fixes to STL Added scene2

--- src/main.cpp
+++ src/main.cpp
@@ -20,40 +20,75 @@ int main() {
     srand(time(NULL));
 
     PPM p = PPM();
-    
-    // Image test = p.read("/home/yo/Escritorio/GRAF/HDR PPM files/forest_path.ppm");
-    
-    // Image output = gammaCurve(test, 0.25);
 
-    Plane left = Plane(1,Vec3(1,0,0),RGB(255,0,0));
-    Plane right = Plane(1,Vec3(-1,0,0),RGB(0,255,0));
-    Plane floor = Plane(1,Vec3(0,1,0));
-    Plane ceiling = Plane(1,Vec3(0,-1,0));
+    //========================= SCENE 1 =========================
+    // Plane left = Plane(1,Vec3(1,0,0),RGB(255,0,0));
+    // Plane right = Plane(1,Vec3(-1,0,0),RGB(0,255,0));
+    // Plane floor = Plane(1,Vec3(0,1,0));
+    // Plane ceiling = Plane(1,Vec3(0,-1,0));
+    // Plane back = Plane(1,Vec3(0,0,-1),RGB(10,10,10));
+    
+    // Sphere A = Sphere(Point(-0.5,-0.7,0.25),Vec3(0,0.6,0),Point(-0.2,-0.7,0.25),RGB(255,255,0));
+    // Sphere B = Sphere(Point(0.5,-0.7,-0.25),Vec3(0,0.6,0),Point(0.8,-0.7,0.25),RGB(255,0,127));
+    // // Triangle t = Triangle(Point(0.5,-0.5,-0.5),Point(0,0.5,-0.5),Point(-0.5,-0.5,-0.5),RGB(0,255,0));
+
+    // Camera camera = Camera(Point(0,0,-3.5),Vec3(0,1,0),Vec3(-1,0,0),Vec3(0,0,3),1024,1024);
+
+    // PointLight pl = PointLight(Point(0,0.5,0),RGB(255,255,255));
+
+    // Scene sc = Scene();
+    // sc.addP(left);
+    // sc.addP(right);
+    // sc.addP(floor);
+    // sc.addP(ceiling);
+    // sc.addP(back);
+    // sc.addP(A);
+    // sc.addP(B);
+    // // sc.addP(t);
+    // sc.addL(pl);
+
+
+
+    //========================= SCENE 2 =========================
     Plane back = Plane(1,Vec3(0,0,-1),RGB(10,10,10));
+    Plane ceiling = Plane(1,Vec3(0,-1,0));
+    STL stlTest = STL("test.stl",Point(0,0,0),0.01,RGB(0,255,127));
+    // STL stlTest = STL("bulbasur.stl",Point(0,0,0),0.01,RGB(0,255,127));
+
+    Vec3 f = Vec3(0,0,5);
+    Vec3 u = Vec3(0,1,0);
+    Vec3 l = Vec3(-1,0,0);
+
+    Transformation t1 = YRotationTransform(0.25);
+    Transformation t2 = XRotationTransform(-0.175);
+    f.applyTransformation(t1);
+    f.applyTransformation(t2);
+    u.applyTransformation(t1);
+    u.applyTransformation(t2);
+    l.applyTransformation(t1);
+    l.applyTransformation(t2);
     
-    Sphere A = Sphere(Point(-0.5,-0.7,0.25),Vec3(0,0.6,0),Point(-0.2,-0.7,0.25),RGB(255,255,0));
-    Sphere B = Sphere(Point(0.5,-0.7,-0.25),Vec3(0,0.6,0),Point(0.8,-0.7,0.25),RGB(255,0,127));
+    Camera camera = Camera(Point(-1,-0.5,-3.5),u,l,f,1024,1024);
 
-    // Triangle t = Triangle(Point(0.5,-0.5,-0.5),Point(0,0.5,-0.5),Point(-0.5,-0.5,-0.5),RGB(0,255,0));
-
-    STL stlTest = STL("bulbasur.stl",Point(0,0,0),0.01,RGB(0,255,127));
-    // STL test = STL("teapot.stl",Point(0,0,0),0.1,RGB(0,255,127));
-
-    Camera camera = Camera(Point(0,0,-3.5),Vec3(0,1,0),Vec3(-1,0,0),Vec3(0,0,3),1024,1024);
-
-    PointLight pl = PointLight(Point(0,0.5,0),RGB(255,255,255));
+    PointLight pl0 = PointLight(Point(-5,0.5,0),RGB(255,255,255));
+    PointLight pl1 = PointLight(Point(-5,0.5,-5),RGB(255,255,255));
+    // PointLight pl2 = PointLight(Point(5,0.5,0),RGB(255,255,255));
+    // PointLight pl3 = PointLight(Point(5,0.5,5),RGB(255,255,255));
 
     Scene sc = Scene();
-    sc.addP(left);
-    sc.addP(right);
-    sc.addP(floor);
-    sc.addP(ceiling);
-    sc.addP(back);
-    sc.addP(A);
-    sc.addP(B);
-    // sc.addP(t);
+    
     sc.addP(stlTest);
-    sc.addL(pl);
+    sc.addP(back);
+    sc.addP(ceiling);
+
+    sc.addL(pl0);
+    sc.addL(pl1);
+    // sc.addL(pl2);
+    // sc.addL(pl3);
+
+
+
+
     
     auto start = chrono::high_resolution_clock::now();
     Image output = camera.render(sc,1);
